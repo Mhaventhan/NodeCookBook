fb02926f2c6254cd13b15287832403162a6aaa9c
“Mhaventhan” <mhaventhan@gmail.com>
2018-09-24 15:42:36 +0100

    initial commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+at_exit { ChefSpec::Coverage.report! }
+# touch creates a rport of the tests passed and the covarage of the tests ran that passed 
